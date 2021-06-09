01146bc06119a6dfd9e95dd44b1432242a4005b0
SSa1nt <39455909+SSa1nt@users.noreply.github.com>
2021-06-09 11:48:38 +0100

    portal

--- GP_Project/Assets/Scripts/Portal.cs
+++ GP_Project/Assets/Scripts/Portal.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour,BoxReferenced
 {
+
+    [Header("Portal Settings")]
     [SerializeField]
     private Portal[] myDestinations;
+    [SerializeField]
+    private Transform myTeleportPoint;
+    [SerializeField]
+    private Color colorDeactivated;
 
 
+    public Transform TeleportPoint => myTeleportPoint;
+    public bool isWorking => isActive;
+
+    private bool isActive = true;
+
+    private Color defaultColor;
+    private SpriteRenderer mySpriteRenderer;
     private void Awake()
     {
         SettupDestinations();
+
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = mySpriteRenderer.color;
     }
 
     private void Start()
@@ -22,40 +38,51 @@ public class Portal : MonoBehaviour,BoxReferenced
     private void SettupDestinations()
     {
         myDestinations = myDestinations.Where(val => val != this).ToArray();
-
-
-        /*foreach (Portal por in myDestinations)
-        {
-            if (por == this)
-            {
-                myDestinations.
-            }
-        }*/
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            Teleport(collision.transform);
+        }
     }
 
-    public void Deactivate()
+    private void Teleport(Transform victim)  //Remake when home
     {
+        bool willtp = false;
+        int index=0;
+        while (!willtp)
+        {
+            index = Random.Range(0, myDestinations.Length);
+            willtp = myDestinations[index].isWorking;
+        }
 
+        victim.position = myDestinations[index].TeleportPoint.position;
     }
 
-    public void Activate()
+
+    private void Deactivate()
     {
-
+        mySpriteRenderer.color = colorDeactivated;
     }
+
+    private void Activate()
+    {
+        mySpriteRenderer.color = defaultColor;
+    }
+
+
+    //interface stuff
 
     public void DeActivateBox()
     {
-        throw new System.NotImplementedException();
+        Deactivate();
     }
 
     public void ActivateBox()
     {
-        throw new System.NotImplementedException();
+        Activate();
     }
 
     public void AddReference()
